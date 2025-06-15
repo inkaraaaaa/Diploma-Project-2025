941288b989108090de3f9b277adaebb9c15a2e73
Ayana <210103190@>
2025-06-15 16:03:33 +0500

    Resolved merge conflicts and continued rebase

--- vacancies/views.py
+++ vacancies/views.py
@@ -6,15 +6,10 @@ from django.contrib import messages
 from .models import JobListing, Application
 from users.models import UserProfile
 from sendreview.models import Company
-from .forms import ApplicationForm
 
 from django.db.models import Count
 
 # views.py
-
-from django.utils import timezone
-
-from django.utils import timezone
 
 from django.contrib.auth.decorators import login_required
 
@@ -26,14 +21,8 @@ def job_list(request):
     course = request.GET.get('course', '')
     companies_selected = request.GET.getlist('company')
 
-    # Обновляем статусы вакансий
-    for job in JobListing.objects.all():
-        job.update_status_if_needed()
+    jobs = JobListing.objects.all()
 
-    # Показываем только open-вакансии
-    jobs = JobListing.objects.filter(status='open')
-
-    # Дополнительные фильтры
     if title:
         jobs = jobs.filter(title__icontains=title)
     if job_type:
@@ -53,7 +42,7 @@ def job_list(request):
         'companies_selected': companies_selected,
     })
 
-# views.py
+
 @login_required(login_url='signin')
 def job_list_view(request):
     job_types = request.GET.getlist('type')
@@ -67,25 +56,12 @@ def job_list_view(request):
 @login_required
 def apply_job(request, job_id):
     job = get_object_or_404(JobListing, id=job_id)
-    student = request.user
+    student = request.user  # это уже UserProfile, если ты заменил AUTH_USER_MODEL
 
     if Application.objects.filter(student=student, job=job).exists():
         messages.warning(request, 'You have already applied for this job.')
-        return redirect('job_detail', job_id=job.id)
-
-    if request.method == 'POST':
-        form = ApplicationForm(request.POST)
-        if form.is_valid():
-            application = form.save(commit=False)
-            application.student = student
-            application.job = job
-            application.save()
-            messages.success(request, 'Application submitted successfully.')
-            return redirect('job_detail', job_id=job.id)
     else:
-        form = ApplicationForm()
+        Application.objects.create(student=student, job=job)
+        messages.success(request, 'Application submitted successfully.')
 
-    return render(request, 'company/apply_form.html', {'job': job, 'form': form})
-
-def success_view(request):
-    return render(request, 'successent.html')
+    return redirect('job_detail', job_id=job.id)
